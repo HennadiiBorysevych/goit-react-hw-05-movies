7517380de050cbc31688ecbfd3d32c3539eb48ff
Hennadii Borysevych <genuch3@gmail.com>
2023-04-30 19:47:57 +0300

    basename added

--- src/API.js
+++ src/API.js
@@ -1,6 +1,5 @@
 const KEY = '?api_key=9039ea298d3f54d722d1b83eac98c5e3';
 
-
 export const trendingMovies = async () => {
   const response = await fetch(
     `https://api.themoviedb.org/3/trending/movie/day${KEY}`
